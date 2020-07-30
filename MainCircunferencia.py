7f3fa6cc5b8f1981d1edc2f8850c8a499be0702a
verduzco <averduzco@itcolima.edu.mx>
2020-07-30 12:14:44 -0500

    commitdel30072020

---
+++ MainCircunferencia.py
@@ -0,0 +1,15 @@
+"""
+Nombre: MainCircunferencia.py
+Objetivo: muestra como trabajar en objetos con python
+Autor:
+Fecha:30 de julio de 2020
+"""
+
+
+from Circunferencia import Circunferencia
+
+
+c = Circunferencia(2,3, 10)
+print(c.toString())
+d = Circunferencia(1,10,2)
+print(d.toString())
